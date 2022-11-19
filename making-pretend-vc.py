f96f630129ae8f28d553cc428a57d17d7c04ea54
shearichard <rshea@thecubagroup.com>
2022-11-19 22:44:05 +1300

    Tidy up and add comments to making-pretend-vc.py

--- making-pretend-vc.py
+++ making-pretend-vc.py
@@ -18,25 +18,31 @@ def make_key_pair(for_who):
 
 
 def main():
-    #
+    # Make key pairs for the Verified Credential Issuer and Verified Credential Holder
     issuer_key_dir = make_key_pair('Issuer')
     holder_key_dir= make_key_pair('Holder')
-    #
+
+    # Make a PyNacl Box for Issuer and Holder (https://pynacl.readthedocs.io/en/latest/public/#nacl-public-box)
     holder_box = Box(holder_key_dir['secretkey'], issuer_key_dir['publickey'])
-    #
+    issuer_box = Box(issuer_key_dir['secretkey'], holder_key_dir['publickey'])
+
+    # Create Verified Credential and encrypted them using holders public key
     verified_cred_as_bytes = make_verified_credential()
     encrypted_verified_cred = holder_box.encrypt(verified_cred_as_bytes)
     #
-    print(verified_cred_as_bytes)
-    print(encrypted_verified_cred)
-    #
-    issuer_box = Box(issuer_key_dir['secretkey'], holder_key_dir['publickey'])
-    #
+    # Unencrypt Verified Credential to demonstrate the fully cycle 
     unencrypted_verified_cred_A = issuer_box.decrypt(encrypted_verified_cred)
-    #
+    
+    #Dump intermediate and final data
+    print("")
+    print("Verified Credential before encryption ...")
+    print(verified_cred_as_bytes)
+    print("")
+    print("Verified Credential after encryption ...")
+    print(encrypted_verified_cred)
+    print("")
+    print("Verified Credential after decryption ...")
     print(unencrypted_verified_cred_A.decode('utf-8'))
-    #
-    #
 
 
 if __name__ == "__main__":

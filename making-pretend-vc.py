ff2a5006c3ee39e5f249e50cb11989a20245ce26
shearichard <rshea@thecubagroup.com>
2022-11-19 23:42:54 +1300

    Fix semantic defect and tidy up in make-pretend-vc.py

--- making-pretend-vc.py
+++ making-pretend-vc.py
@@ -1,7 +1,8 @@
-import nacl.utils
-from nacl.public import PrivateKey, Box
 import pprint
 import base64
+
+import nacl.utils
+from nacl.public import PrivateKey, Box
 
 def make_verified_credential():
     verified_cred_as_json = {'name': 'Alice', 'age': 22}
@@ -22,18 +23,30 @@ def main():
     issuer_key_dir = make_key_pair('Issuer')
     holder_key_dir= make_key_pair('Holder')
 
-    # Make a PyNacl Box for Issuer and Holder (https://pynacl.readthedocs.io/en/latest/public/#nacl-public-box)
-    holder_box = Box(holder_key_dir['secretkey'], issuer_key_dir['publickey'])
-    issuer_box = Box(issuer_key_dir['secretkey'], holder_key_dir['publickey'])
-
-    # Create Verified Credential and encrypted them using holders public key
+    # Make a PyNacl Box for the Issuer 
+    #(https://pynacl.readthedocs.io/en/latest/public/#nacl-public-box)
+    issuer_box = Box(   private_key=issuer_key_dir['secretkey'], 
+                        public_key=holder_key_dir['publickey'])
+     
+    # Create Verified Credential and encrypt it 
     verified_cred_as_bytes = make_verified_credential()
-    encrypted_verified_cred = holder_box.encrypt(verified_cred_as_bytes)
-    #
+    encrypted_verified_cred = issuer_box.encrypt(verified_cred_as_bytes)
+     
+    ############################################################################
+    # Now go into reverse
+    ############################################################################
+
+    # Make a PyNacl Box for the Holder 
+    #(https://pynacl.readthedocs.io/en/latest/public/#nacl-public-box)
+    holder_box = Box(   private_key=holder_key_dir['secretkey'], 
+                         public_key=issuer_key_dir['publickey'])
+    # 
     # Unencrypt Verified Credential to demonstrate the fully cycle 
-    unencrypted_verified_cred_A = issuer_box.decrypt(encrypted_verified_cred)
+    unencrypted_verified_cred_A = holder_box.decrypt(encrypted_verified_cred)
     
+    ############################################################################
     #Dump intermediate and final data
+    ############################################################################
     print("")
     print("Verified Credential before encryption ...")
     print(verified_cred_as_bytes)
